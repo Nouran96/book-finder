018229dcdc3eee4bbb0f78e088f597aa050e6762
Nouran Samy <nourans.96@yahoo.com>
2020-11-23 12:04:57 +0200

    Add loader when fetching on slow network

--- src/containers/BooksList/BooksList.jsx
+++ src/containers/BooksList/BooksList.jsx
@@ -1,20 +1,26 @@
 import BookCard from "../../components/BookCard/BookCard";
 import { connect } from "react-redux";
 
-const BooksList = ({ books }) => {
+const BooksList = ({ books, fetching }) => {
   return (
     <div className="d-flex flex-wrap justify-content-center">
-      {books && books.length > 0 ? (
-        books.map((book) => <BookCard key={book.id} book={book.volumeInfo} />)
-      ) : (
-        <p className="text-center">No Books Available</p>
-      )}
+      {
+        // If fetching data, show this message otherwise check the books returned
+        fetching ? (
+          <p className="text-center">Fetching...</p>
+        ) : books && books.length > 0 ? (
+          books.map((book) => <BookCard key={book.id} book={book.volumeInfo} />)
+        ) : (
+          <p className="text-center">No Books Available</p>
+        )
+      }
     </div>
   );
 };
 
 const mapStateToProps = (state) => ({
   books: state.books,
+  fetching: state.fetching,
 });
 
 export default connect(mapStateToProps)(BooksList);
--- src/store/reducers/books.jsx
+++ src/store/reducers/books.jsx
@@ -1,15 +1,19 @@
-import { BOOKS_ADD, BOOKS_EMPTY } from "../types/books";
+import { BOOKS_ADD, BOOKS_FETCH } from "../types/books";
 
 const INITIAL_STATE = {
   books: [],
+  fetching: false,
 };
 
 const addBooks = (state, action) => ({
   books: [...action.payload.books],
+  fetching: false,
 });
 
 const booksReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case BOOKS_FETCH:
+      return { books: [...state.books], fetching: true };
     case BOOKS_ADD:
       return addBooks(state, action);
     default:
